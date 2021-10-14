f2af69d5318b77187337db5ef59c1b2fa96c9628
Jamie Hughes <jamie.hughes42@btinternet.com>
2021-10-14 11:19:01 +0100

    Bingo V2

---
+++ bingo/js/data.js
@@ -0,0 +1,37 @@
+var headerText = "Bingo";
+
+var footerText = "GLHF";
+
+var winText = "Winner";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var winSnd = new Audio("audio/win.mp3");
+
+var JSONBingo = {"squares": [
+        {"square": "Mentions 'Youth Club'"},
+	{"square": "Claps hands or hits something to get everyone quiet"},
+	{"square": "Tells an inappropriate joke"},
+	{"square": "'Bring the noise level down'"},
+	{"square": "'I am Deaf'"},
+	{"square": "Mentions Barry"},
+	{"square": "'We will do that next week'"},
+	{"square": "Express his love of Teams"},
+	{"square": "Express his hate of Teams"},
+	{"square": "'CBeebies'"},
+	{"square": "Forgot when break is"},
+	{"square": "Makes someone stay behind after class"},
+	{"square": ""},
+	{"square": ""},
+	{"square": ""},
+	{"square": ""},
+	{"square": ""},
+	{"square": ""},
+	{"square": ""},
+	{"square": ""},
+	{"square": ""},
+	{"square": ""},
+	{"square": ""},
+	{"square": ""}
+    ]
+};
